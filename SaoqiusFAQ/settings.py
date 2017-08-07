092854f3e7fe361ee598221000d79918b9d8e133
yangxiang <18614085772@163.com>
2017-08-08 00:16:37 +0800

    saoqi33

--- SaoqiusFAQ/settings.py
+++ SaoqiusFAQ/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SaoqiusFAQ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,  'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,"static"),
+)
--- web/urls.py
+++ web/urls.py
@@ -18,6 +18,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^web/', include('web.urls')),
-    url(r'^backend/', include('backend.urls')),
 ]
